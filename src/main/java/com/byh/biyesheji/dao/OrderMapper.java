134ee73f62059381679a715f7af0a0521b39a1ad
sunming <@sunming>
2024-03-16 17:52:00 +0800

    培训申请页面

--- src/main/java/com/byh/biyesheji/dao/OrderMapper.java
+++ src/main/java/com/byh/biyesheji/dao/OrderMapper.java
@@ -1,5 +1,6 @@
 package com.byh.biyesheji.dao;
 
+import com.byh.biyesheji.entity.ApplicationDocuments;
 import com.byh.biyesheji.pojo.Order;
 import com.byh.biyesheji.pojo.OrderExample;
 
@@ -8,5 +9,12 @@ import java.util.List;
 public interface OrderMapper extends CrudDao<Order>{
 
     List<Order> selectByExample(OrderExample example);
+
+    /**
+     * 根据条件查询数据
+     * @param order
+     * @return
+     */
+    List<Order>  queryAll(Order order);
 
 }
